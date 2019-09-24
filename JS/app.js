b407446df701f39b71ccef222d07952ea62e3c91
PlateauGao <173358869@qq.com>
2019-09-24 15:28:53 -0700

    V2.0

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+function Store(initialStock) {
+    this.stock = initialStock;
+    this.cart = [];
+    this.addItemToCart = function(itemName) {
+
+    }
+    this.removeItemFromCart = function(itemName) {
+
+    }
+}
+var products;
