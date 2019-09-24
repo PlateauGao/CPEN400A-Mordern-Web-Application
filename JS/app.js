bf83bc8fa139b968bd5be05f07e7beda447fc5da
PlateauGao <173358869@qq.com>
2019-09-24 16:19:45 -0700

    V2.02

--- JS/app.js
+++ JS/app.js
@@ -8,20 +8,13 @@ function Store(initialStock) {
 
     }
 }
-var productsArray = [
-    "Box1",
-    "Box2",
-    "Clothes1",
-    "Clothes2",
-    "Jeans",
-    "KeyboardCombo",
-    "Keyboard",
-    "Mice",
-    "PC1",
-    "PC2",
-    "PC3",
-    "Tent"
+var keys = [
+    "Box1", "Box2", "Clothes1", "Clothes2", "Jeans", "KeyboardCombo", "Keyboard", "Mice", "PC1", "PC2", "PC3", "Tent"
 ];
+var labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+var prices = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+var quantities = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+var imageUrl = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 
 function Product(label, imageUrl, price, quantity) {
     this.label = label;
@@ -29,3 +22,17 @@ function Product(label, imageUrl, price, quantity) {
     this.imageUrl = imageUrl;
     this.quantity = quantity;
 }
+var products = {}; /*associate array */
+
+for (let i = 0; i != 20; i++) {
+    products[keys[i]] = new Product(labels[i], prices[i], imageUrl[i], quantities[i]);
+}
+/*
+for (key in products) {
+    val = products[key];
+    console.log(val.price);
+    console.log(val.label);
+}
+*/
+
+var store = new Store(products);
