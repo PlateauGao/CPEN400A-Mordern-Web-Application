e72731f33d9e654c619b131bae2910a5de7a1a11
PlateauGao <173358869@qq.com>
2019-09-24 15:39:00 -0700

    V2.01

--- JS/app.js
+++ JS/app.js
@@ -8,4 +8,24 @@ function Store(initialStock) {
 
     }
 }
-var products;
+var productsArray = [
+    "Box1",
+    "Box2",
+    "Clothes1",
+    "Clothes2",
+    "Jeans",
+    "KeyboardCombo",
+    "Keyboard",
+    "Mice",
+    "PC1",
+    "PC2",
+    "PC3",
+    "Tent"
+];
+
+function Product(label, imageUrl, price, quantity) {
+    this.label = label;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.quantity = quantity;
+}
