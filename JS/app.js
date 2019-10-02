d05b1e204b39e5b86ec25de41965c4d80c0d504d
PlateauGao <173358869@qq.com>
2019-10-02 16:09:27 -0700

    fix bugs

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@ var keys = [
     "Box1", "Box2", "Clothes1", "Clothes2", "Jeans", "KeyboardCombo", "Keyboard", "Mice", "PC1", "PC2", "PC3", "Tent"
 ];
 
-var labels = keys;
+var labels = ["White Box", "Boxes Set", "Red Dress", "T-shirt", "Jeans", "Red Keyboard", "Colorful Keyboard", "Mouse", "Dell PC", "Intel PC Set", "Intel PC", "Tent"];
 
 var imageUrls = [
     "https://github.com/PlateauGao/assignments/blob/master/assignments/images/Box1_$10.png?raw=true",
@@ -19,15 +19,15 @@ var imageUrls = [
     "https://github.com/PlateauGao/assignments/blob/master/assignments/images/Tent_$100.png?raw=true"
 ];
 
-var prices = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+var prices = [10, 5, 20, 30, 50, 40, 20, 20, 350, 400, 300, 100];
 var quantities = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 
-var Store = function (initialStock) {
+var Store = function(initialStock) {
     this.stock = initialStock;
     this.cart = {};
 };
 
-Store.prototype.addItemToCart = function (itemName) {
+Store.prototype.addItemToCart = function(itemName) {
     if (this.stock[itemName].quantity === 0) {
         alert(itemName + " sold out.");
         return;
@@ -43,7 +43,7 @@ Store.prototype.addItemToCart = function (itemName) {
     alert("Done.");
 };
 
-Store.prototype.removeItemFromCart = function (itemName) {
+Store.prototype.removeItemFromCart = function(itemName) {
     if (!this.cart.hasOwnProperty(itemName)) {
         alert("No " + itemName + " in the cart.");
         return;
@@ -59,7 +59,7 @@ Store.prototype.removeItemFromCart = function (itemName) {
 
 var products = {};
 
-var Product = function (label, imageUrl, price, quantity) {
+var Product = function(label, imageUrl, price, quantity) {
     this.label = label;
     this.imageUrl = imageUrl;
     this.price = price;
@@ -88,25 +88,25 @@ function removeFromCart(itemName) {
 function showCart(cart) {
     var itemList = "";
     for (var item in cart)
-        itemList += item + ": " + cart[item] + "\n";
+        itemList += store.stock[item].label + ": " + cart[item] + "\n";
     inactiveTime = 0;
     alert(itemList);
 }
 
-keys.forEach(function (itemName) {
+keys.forEach(function(itemName) {
     var addButtonID = "add" + itemName;
     var removeButtonID = "remove" + itemName;
     document.getElementById(addButtonID).onclick = addToCart(itemName);
     document.getElementById(removeButtonID).onclick = removeFromCart(itemName);
 });
 
-document.getElementById('btn-show-cart').onclick = function () {
+document.getElementById('btn-show-cart').onclick = function() {
     showCart(store.cart);
 };
 
 var inactiveTime = 0;
 
-setInterval(function () {
+setInterval(function() {
     if (inactiveTime < 5)
         inactiveTime++;
     else {
--- temp.js
+++
@@ -1,7 +0,0 @@
-var keys = [
-    "Box1", "Box2", "Clothes1", "Clothes2", "Jeans", "KeyboardCombo", "Keyboard", "Mice", "PC1", "PC2", "PC3", "Tent"
-];
-keys.forEach(function(e) {
-    console.log("a" + e)
-
-})
